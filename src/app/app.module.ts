104987b2f5adf4867ea3cdb0980d926173b87604
Paolo Dalex <paolodalex96@gmail.com>
2022-10-14 00:08:50 +0200

    deleted a lot of useless files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
-import './training_javascript'
-import './training_typescript';
-import { ApplicaColoreDirective } from './applica-colore.directive';
-import { RiduciLunghezzaPipe } from './riduci-lunghezza.pipe';
-import { FiltraArrayPipe } from './filtra-array.pipe';
 import { ProductsComponent } from './products/products.component';
 import { HomeComponent } from './home/home.component';
 import { PaginaNonTrovataComponent } from './pagina-non-trovata/pagina-non-trovata.component';
