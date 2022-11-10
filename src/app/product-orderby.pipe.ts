3e763ca24f293bff5a37d2e099f821f2d343408b
Paolo Dalex <paolodalex96@gmail.com>
2022-11-10 23:30:10 +0100

    feat: starting implementation of orderby pipe

--- src/app/product-orderby.pipe.ts
+++ src/app/product-orderby.pipe.ts
@@ -6,14 +6,35 @@ import { Product } from './dati/product.data';
 })
 export class ProductOrderbyPipe implements PipeTransform {
 
-  transform(value: Product[], filter :String, order ?:String): Product[] {
+  transform(value: Product[], filter :String, order ?:String, category ?:String): Product[] {
 
     switch(filter) {
       case 'price':
-        return value.sort((a, b) => (Number(a.price) < Number(b.price)) ? 1 : -1)
+        if(order === "cres") {
+          return value.sort((a, b) => (Number(a.price) < Number(b.price)) ? 1 : -1)
+        }
+        else {
+          return value.sort((a, b) => (Number(a.price) > Number(b.price)) ? 1 : -1)
+        }
 
       case 'review':
-        return value.sort((a, b) => (Number(a.reviews) < Number(b.reviews) ? 1 : -1))
+        if(order === "cres") {
+          return value.sort((a, b) => (Number(a.reviews) < Number(b.reviews) ? 1 : -1))
+        }
+        else {
+          return value.sort((a, b) => (Number(a.reviews) > Number(b.reviews) ? 1 : -1))
+        }
+
+      case 'category':
+        return value.filter(p => p.category === category)
+
+      case 'stock':
+        if(order === "cres") {
+          return value.sort((a, b) => (a.stock < b.stock) ? 1 : -1)
+        }
+        else {
+          return value.sort((a, b) => (a.stock > b.stock) ? 1 : -1)
+        }
 
       default:
         return value
